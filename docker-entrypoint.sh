b5e4e545815c4a8199e72149f5c83ea7313ee832
Alexander Krupenkin <mail@akru.me>
2017-03-22 10:09:00 +0300

    Entrypoint fixes

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-. /tmp/build/setup.sh
+. /tmp/build/devel/setup.sh
 export ROS_MASTER_URI="http://${MASTER:-localhost}:11311"
 export HOSTNAME=$(cat /etc/hostname)
 exec roslaunch drone_adapter_mavlink solo.launch
